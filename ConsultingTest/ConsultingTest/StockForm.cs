790d0855813fbd639fb0a847cce1e3023e8609ed
muhammedikbal <72975816+Muhammedikbalkipoglu@users.noreply.github.com>
2021-08-26 16:54:24 +0300

    Proje - Başlangıç
    .net Core WinForm proje alt yapısı.

---
+++ ConsultingTest/ConsultingTest/StockForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ConsultingTest
+{
+    public partial class StockForm : Form
+    {
+        public StockForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
